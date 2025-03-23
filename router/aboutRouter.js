a3f1caf9e97bd184480c3c452e655921420997b5
Rotem Sharon <rotemts10@gmail.com>
2025-03-23 15:48:32 +0200

    Adding about pages and adding Hebrew option

--- router/aboutRouter.js
+++ router/aboutRouter.js
@@ -5,7 +5,32 @@ const path = require('path')
 // router.use(express.json())
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../views/about.html'))
+    res.sendFile(path.join(__dirname + '/../views/about/about.html'))
+})
+
+router.get('/front_end', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../views/about/frontEnd.html'))
+})
+router.get('/front_end/hebrew', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../views/about/hebrew/frontEndHebrew.html'))
+})
+
+router.get('/back_end', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../views/about/backEnd.html'))
+})
+router.get('/back_end/hebrew', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../views/about/hebrew/backEndHebrew.html'))
+})
+
+router.get('/database', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../views/about/database.html'))
+})
+router.get('/database/hebrew', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../views/about/hebrew/databaseHebrew.html'))
+})
+
+router.get('/developer', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../views/about/developer.html'))
 })
 
 module.exports = router
